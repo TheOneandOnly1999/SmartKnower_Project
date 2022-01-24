3cca60cdea08974cf553c1dcec80a16826edef45
TheOneandOnly1999 <86534139+TheOneandOnly1999@users.noreply.github.com>
2022-01-24 13:47:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ st.title('Spam Ham Deployment')
 test_model=joblib.load('spam_ham')
 ip=st.text_input('Enter your message')
 op=test_model.predict([ip])
-if st.button('Predict')
+if st.button('Predict'):
   st.title(op)
  
