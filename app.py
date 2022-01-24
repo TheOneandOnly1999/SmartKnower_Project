9a13bcee09944cb3a071efc7ed32dfad2a16eb82
TheOneandOnly1999 <86534139+TheOneandOnly1999@users.noreply.github.com>
2022-01-24 13:10:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-
+%%writefile app.py
+import streamlit as st
+import joblib
+st.title('Spam Ham Deployment')
+test_model=joblib.load('spam_ham')
+ip=st.text_input('Enter your message')
+op=test_model.predict([ip])
+if st.button('Predict')
+  st.title(op)
